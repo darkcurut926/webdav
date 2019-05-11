daeebe4627c9d5db63ce62abdd01e76161f14118
darkcurut926 <50315810+darkcurut926@users.noreply.github.com>
2019-05-12 06:59:34 +0700

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,5 @@
+clear
+cowsay -f eyes " Selamat Menikmati Tools Webdav Saya "
+figlet -f slant "Moreno77"
+read -p " [ ? ] Masukan Web Target => " web;
+read -p " [ ? ] Masukan Alamat Sc Deface =>" sc;
